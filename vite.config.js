9b05fc166d9e7c2c78b648ed8cdd466dff2d07d4
PRIYAM <you@example.com>
2025-03-09 22:32:24 +0530

    removed server proxy from vite config

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  server: {
-    proxy: {
-      '/api': 'https://e-commerce-backend-production-4354.up.railway.app',
-    },
-  },
 })
